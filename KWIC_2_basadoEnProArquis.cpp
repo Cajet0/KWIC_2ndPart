ea0e961703c6ace5c783ac6a7537e63117185c7a
Mario Sergio Fuentes <msf1013@gmail.com>
2017-04-19 17:41:52 -0500

    Wrapped functionality in a class and fixed a couple of bugs

--- KWIC_2_basadoEnProArquis.cpp
+++ KWIC_2_basadoEnProArquis.cpp
@@ -30,176 +30,185 @@ pittsburgh is beautiful
 white clouds are
 */
 
-vector<string> glbVecSInputStrings;
-vector<string> glbVecSCircularIt;
-vector<string> glbVecSStopWords;
+class KWIC {
 
+    vector<string> glbVecSInputStrings;
+    vector<string> glbVecSCircularIt;
+    vector<string> glbVecSStopWords;
 
-string transLowerC(string sLine) {
-    for (int iI = 0; iI < sLine.size(); iI++) {
-        sLine[iI] = tolower(sLine[iI]);
-    }
-    return sLine;
-}
+    public:
 
-// Compara cadena recibida con la lista de palabras STOP para tomarla en cuenta o no
-bool removeWord(string sWord) {
-    for (int iIt = 0; iIt < glbVecSStopWords.size(); iIt++) {
-        if (strcmp(sWord.c_str(), glbVecSStopWords[iIt].c_str()) == 0)
-            return true;
-    }
-
-    return false;
-}
-
-// Recibe un vector para imprimir su contenido de manera enumerada
-void printWithID(vector<string> listaTexto) {
-    for (int iIte = 0; iIte < listaTexto.size(); iIte++) {
-        cout << iIte << " - " << listaTexto[iIte] << endl;
-    }
-}
-
-string cricularShift(string sLine) {
-    int iFindSpace = sLine.find(' ');
-    if(iFindSpace != -1){
-        sLine = sLine.substr(iFindSpace + 1) + " " + sLine.substr(0, iFindSpace);
-    }
-    return sLine;
-}
-
-// Funcion que elimina espacios en blanco innecesarios que separe a las palabras
-string removeExtraSpaces(string sLine) {
-    string sFormatLine = "";
-
-    while (sLine != "") {
-        if (sLine.find(" ") == -1) {
-            //Ya no se encontraro espacios en blanco
-            sFormatLine.append(sLine);
-            break;
-        }
-        else {
-            // Se identifica una palabra en la linea de texto
-            string sWord = sLine.substr(0, sLine.find(" ") + 1);
-            // Con esto se eliminan espacios en blanco que sobraran entre la separacion de palabras y se descartan palabras STOP
-            if (sWord != " " && !removeWord(sWord.substr(0, sWord.length() - 1))) {
-                    sFormatLine.append(sWord);
-            }
-            sLine.erase(0, sLine.find(" ") + 1);
-        }
-    }
-
-    return sFormatLine;
-}
-
-void getInput() {
-    cout << "Ingresa cada una de las palabras STOP (Una palabra por renglon)" << endl;
-    cout << "Finaliza la accion ingresando una cadena vacia" << endl;
-    string sLine;
-    getline(cin, sLine);
-    sLine = transLowerC(sLine);
-
-    // el input termina cuando escriba ya no ingrese texto
-    while(sLine!="") {
-        sLine = removeExtraSpaces(sLine);
-        glbVecSStopWords.push_back(sLine);
-
-        getline(cin, sLine);
-        sLine = transLowerC(sLine);
-    }
-
-    cout << endl << "Ingresa lineas de texto a solucionar. Ingrese 'end' para finalizar la entrada." << endl;
-    getline(cin, sLine);
-    sLine = transLowerC(sLine);
-    // el input termina cuando escriba "end"
-    while(sLine!="end") {
-        if (sLine != "") {
-            sLine = removeExtraSpaces(sLine);
-            glbVecSInputStrings.push_back(sLine);
-        }
-
-        getline(cin, sLine);
-        sLine = transLowerC(sLine);
-    }
-}
-
-void deleteInputLines() {
-    int iPosRenglon = -10;
-
-    while (iPosRenglon != -1) {
-        cout << endl << "Lineas de entrada ingresada por usuario: " << endl;
-        printWithID(glbVecSInputStrings);
-
-        cout << "Ingrese numero de renglon si desea eliminarlo como entrada" << endl;
-        cout << "Ingrese -1 para proseguir" << endl;
-        cin >> iPosRenglon;
-
-        if (iPosRenglon > -1 && iPosRenglon < glbVecSInputStrings.size()) {
-            glbVecSInputStrings.erase(glbVecSInputStrings.begin() + iPosRenglon);
-        }
-    }
-}
-
-void getShifts() {
-    for (int iItVect = 0; iItVect < glbVecSInputStrings.size(); iItVect++) {
-        int iContWords = 0;
-        string sLine = glbVecSInputStrings[iItVect];
+    string transLowerC(string sLine) {
         for (int iI = 0; iI < sLine.size(); iI++) {
-            if (sLine[iI] == ' ')
-                iContWords++;
+            sLine[iI] = tolower(sLine[iI]);
         }
-        iContWords++;
-
-        for (int iI = 0; iI < iContWords; iI ++) {
-            glbVecSCircularIt.push_back(sLine);
-            sLine = cricularShift(sLine);
-        }
+        return sLine;
     }
-}
 
-void sortData() {
-    int iRespuesta;
-    cout << "Ingrese numero de tipo de orden deseado: " << endl;
-    cout << "1) Ascendente  -  2) Descendente" << endl;
-    cin >> iRespuesta;
+    // Compara cadena recibida con la lista de palabras STOP para tomarla en cuenta o no
+    bool removeWord(string sWord) {
+        for (int iIt = 0; iIt < glbVecSStopWords.size(); iIt++) {
+            if (strcmp(sWord.c_str(), glbVecSStopWords[iIt].c_str()) == 0) {
+                return true;
+            }
+        }
 
-    sort(glbVecSCircularIt.begin(), glbVecSCircularIt.end());
+        return false;
+    }
 
-    if (iRespuesta == 2)
-        reverse(glbVecSCircularIt.begin(), glbVecSCircularIt.end());
-}
-
-void deleteOutputLines() {
-    int iPosRenglon = -10;
-
-    while (iPosRenglon != -1) {
-        cout << endl << "Lineas de salida generadas: " << endl;
-        printWithID(glbVecSCircularIt);
-
-        cout << "Ingrese numero de renglon si desea eliminar alguna salida" << endl;
-        cout << "Ingrese -1 para proseguir" << endl;
-        cin >> iPosRenglon;
-
-        if (iPosRenglon > -1 && iPosRenglon < glbVecSCircularIt.size()) {
-            glbVecSCircularIt.erase(glbVecSCircularIt.begin() + iPosRenglon);
+    // Recibe un vector para imprimir su contenido de manera enumerada
+    void printWithID(vector<string> listaTexto) {
+        for (int iIte = 0; iIte < listaTexto.size(); iIte++) {
+            cout << iIte << " - " << listaTexto[iIte] << endl;
         }
     }
-}
 
-void printOutput() {
-    cout << endl << "Ordenamientos resultantes: " << endl;
-    for (int iI = 0; iI < glbVecSCircularIt.size(); iI++){
-        cout<<glbVecSCircularIt[iI]<<endl;
+    string cricularShift(string sLine) {
+        int iFindSpace = sLine.find(' ');
+        if(iFindSpace != -1){
+            sLine = sLine.substr(iFindSpace + 1) + " " + sLine.substr(0, iFindSpace);
+        }
+        return sLine;
     }
-}
 
+    // Funcion que elimina espacios en blanco innecesarios que separe a las palabras
+    string removeExtraSpaces(string sLine, bool prueba = false) {
+        string sFormatLine = "";
+        while (sLine != "") {
+            if (sLine.find(" ") == -1) {
+                //Ya no se encontraro espacios en blanco
+                if (!removeWord(sLine)) {
+                    sFormatLine.append(sLine);
+                } else if (sFormatLine.size() > 0) {
+                    sFormatLine = sFormatLine.substr(0, sFormatLine.length() - 1);
+                }
+                break;
+            }
+            else {
+                // Se identifica una palabra en la linea de texto
+                string sWord = sLine.substr(0, sLine.find(" ") + 1);
+
+                // Con esto se eliminan espacios en blanco que sobraran entre la separacion de palabras y se descartan palabras STOP
+                if (sWord != " " && !removeWord(sWord.substr(0, sWord.length() - 1))) {
+                        sFormatLine.append(sWord);
+                }
+                sLine.erase(0, sLine.find(" ") + 1);
+            }
+        }
+        return sFormatLine;
+    }
+
+    void getInput() {
+        cout << "Ingresa cada una de las palabras STOP (Una palabra por renglon)" << endl;
+        cout << "Finaliza la accion ingresando una cadena vacia" << endl;
+        string sLine;
+        getline(cin, sLine);
+        sLine = transLowerC(sLine);
+
+        // el input termina cuando escriba ya no ingrese texto
+        while(sLine!="") {
+            sLine = removeExtraSpaces(sLine);
+            glbVecSStopWords.push_back(sLine);
+
+            getline(cin, sLine);
+            sLine = transLowerC(sLine);
+        }
+
+        cout << endl << "Ingresa lineas de texto a solucionar. Ingrese 'end' para finalizar la entrada." << endl;
+        getline(cin, sLine);
+        sLine = transLowerC(sLine);
+        // el input termina cuando escriba "end"
+        while(sLine!="end") {
+            if (sLine != "") {
+                sLine = removeExtraSpaces(sLine, true);
+                glbVecSInputStrings.push_back(sLine);
+            }
+
+            getline(cin, sLine);
+            sLine = transLowerC(sLine);
+        }
+    }
+
+    void deleteInputLines() {
+        int iPosRenglon = -10;
+
+        while (iPosRenglon != -1) {
+            cout << endl << "Lineas de entrada ingresada por usuario: " << endl;
+            printWithID(glbVecSInputStrings);
+
+            cout << "Ingrese numero de renglon si desea eliminarlo como entrada" << endl;
+            cout << "Ingrese -1 para proseguir" << endl;
+            cin >> iPosRenglon;
+
+            if (iPosRenglon > -1 && iPosRenglon < glbVecSInputStrings.size()) {
+                glbVecSInputStrings.erase(glbVecSInputStrings.begin() + iPosRenglon);
+            }
+        }
+    }
+
+    void getShifts() {
+        for (int iItVect = 0; iItVect < glbVecSInputStrings.size(); iItVect++) {
+            int iContWords = 0;
+            string sLine = glbVecSInputStrings[iItVect];
+            for (int iI = 0; iI < sLine.size(); iI++) {
+                if (sLine[iI] == ' ')
+                    iContWords++;
+            }
+            iContWords++;
+
+            for (int iI = 0; iI < iContWords; iI ++) {
+                glbVecSCircularIt.push_back(sLine);
+                sLine = cricularShift(sLine);
+            }
+        }
+    }
+
+    void sortData() {
+        int iRespuesta;
+        cout << "Ingrese numero de tipo de orden deseado: " << endl;
+        cout << "1) Ascendente  -  2) Descendente" << endl;
+        cin >> iRespuesta;
+
+        sort(glbVecSCircularIt.begin(), glbVecSCircularIt.end());
+
+        if (iRespuesta == 2)
+            reverse(glbVecSCircularIt.begin(), glbVecSCircularIt.end());
+    }
+
+    void deleteOutputLines() {
+        int iPosRenglon = -10;
+
+        while (iPosRenglon != -1) {
+            cout << endl << "Lineas de salida generadas: " << endl;
+            printWithID(glbVecSCircularIt);
+
+            cout << "Ingrese numero de renglon si desea eliminar alguna salida" << endl;
+            cout << "Ingrese -1 para proseguir" << endl;
+            cin >> iPosRenglon;
+
+            if (iPosRenglon > -1 && iPosRenglon < glbVecSCircularIt.size()) {
+                glbVecSCircularIt.erase(glbVecSCircularIt.begin() + iPosRenglon);
+            }
+        }
+    }
+
+    void printOutput() {
+        cout << endl << "Ordenamientos resultantes: " << endl;
+        for (int iI = 0; iI < glbVecSCircularIt.size(); iI++){
+            cout<<glbVecSCircularIt[iI]<<endl;
+        }
+    }
+};
 
 int main() {
-    getInput();
-    deleteInputLines();
-    getShifts();
-    sortData();
-    deleteOutputLines();
-    printOutput();
+    KWIC kwic;
+
+    kwic.getInput();
+    kwic.deleteInputLines();
+    kwic.getShifts();
+    kwic.sortData();
+    kwic.deleteOutputLines();
+    kwic.printOutput();
 
     return 0;
 }
